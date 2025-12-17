bc22b1c4b7b9c03cc5512494bed118e867f03a25
Salaheddine <salahsimbaa@gmail.com>
2025-12-17 17:13:11 +0100

    Brand About

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { assets } from './../assets/assets';
+export default function Navbar(){
+    const [showmenumobile,setshowmenumobile] = useState(false);
+
+
+useEffect(()=>{
+    if(showmenumobile){
+        document.body.style.overflow = 'hidden'
+    }else{
+        document.body.style.overflow = 'auto'
+    }
+    return ()=>{
+        document.body.style.overflow = 'auto'
+    };
+},[showmenumobile])
+
+
+
+    return(
+        <div className="absolute top-0 left-0 w-full z-10" >
+            <div className="container mx-auto flex justify-between items-center
+             py-4 px-6 md:px-20 lg:px-32 bg-transparent" >
+                <img src={assets.logo} alt="" />
+                <ul className="hidden md:flex gap-7 text-white" >
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400">Home</a>
+                    <a href="#About" className="cursor-pointer hover:text-gray-400">About</a>
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400">Projects</a>
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400">Testimonials</a>
+                </ul>
+                <button className="hidden md:block bg-white px-8 py-2
+                 rounded-full">Contact-Us</button>
+                 
+                 <img onClick={()=>setshowmenumobile(true)} src={assets.menu_icon} className="md:hidden w-7 
+                 cursor-pointer" alt="" />
+                 {/* -------------mobile menu------------ */}
+                 <div className={`md:hidden 
+                 ${showmenumobile ? 'fixed w-full': 'hidden'} 
+                  right-0 top-0 bottom-0
+                 overflow-hidden bg-white transition-all`}>
+                    <div className="flex justify-end p-6 cursor-pointer">
+                        <img onClick={()=>setshowmenumobile(false)} src={assets.cross_icon} className="w-6" alt="" />
+                    </div>
+                    <ul className="flex flex-col items-center gap-2 mt-5 px-5 
+                    text-lg font-medium">
+                        <a onClick={()=>setshowmenumobile(false)} href="#Header" className="px-4 py-2 rounded-full inline-block
+                        ">Home</a>
+                        <a onClick={()=>setshowmenumobile(false)} href="#About" className="px-4 py-2 rounded-full inline-block
+                        ">About</a>
+                        <a onClick={()=>setshowmenumobile(false)} href="#Projects" className="px-4 py-2 rounded-full inline-block
+                        ">Projects</a>
+                        <a onClick={()=>setshowmenumobile(false)} href="#Teastimonails" className="px-4 py-2 rounded-full inline-block
+                        ">Testimonails</a>
+                    </ul>
+                 </div>
+            </div>
+        </div>
+    )
+}
