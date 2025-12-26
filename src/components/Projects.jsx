3c26a8ac376e3c5e45bfea264a263615cb4be2f1
Salaheddine <salahsimbaa@gmail.com>
2025-12-26 09:35:37 +0100

    Projects start

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Projects(){
     return(
-        <div>
-            
+        <div className="container mx-auto py-4 pt-20 px-6 md:px-20 
+        ld:px-32 my-20 w-full overflow-hidden" id="Projects">
+            <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center
+             ">Projects <span className="underline underline-offset-4xl decoration-1
+             under font-light" >Completed</span></h1>
+            <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto
+            " >Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
         </div>
     )
 }
