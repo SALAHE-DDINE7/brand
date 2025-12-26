2bf9727fd19712e1efbe938fb0bdeb7236c28f50
Salaheddine <salahsimbaa@gmail.com>
2025-12-26 11:23:59 +0100

    Projects steps

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets, projectsData } from './../assets/assets';
 
 export default function Projects(){
     return(
@@ -9,6 +10,27 @@ export default function Projects(){
              under font-light" >Completed</span></h1>
             <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto
             " >Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+
+            {/* Slider Button */}
+            <div className="flex justify-end items-center mb-8" >
+                <button className="p-3 bg-gray-200 rounded mr-2" aria-label="Previous Project" >
+                    <img src={assets.left_arrow} alt="Previous"/>
+                </button>
+                <button className="p-3 bg-gray-200 rounded mr-2" aria-label="Next Project" >
+                    <img src={assets.right_arrow} alt="Next"/>
+                </button>
+            </div>
+            {/* Projects Container */}
+
+            <div className="overflow-hidden">
+                <div className="flex gap-8 transition-transform duration-500 ease_in_out ">
+                    {projectsData.map((project,index)=>(
+                        <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+                            <img src={project.image} alt={project.title} />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
